7ac1c53e982f54138c102c06d10fde9d7161d70d
Stier Máté <stiermategy@gmail.com>
2021-11-26 11:34:24 +0100

    some changes

---
+++ InAnOut_1/Controllers/SuperUserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAnOut_1.Controllers
+{
+    public class SuperUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
